3b2209bd8eac1665ef79ae9106a71962488b19a6
mayor falomo <mayorfalomo@gmail.com>
2024-06-13 22:13:24 +0100

    updated responsiveness for facilities component

--- components/home/homeFacilities/Facilities.tsx
+++ components/home/homeFacilities/Facilities.tsx
@@ -7,41 +7,44 @@ function Facilities({}: Props) {
   return (
     <div className="bg-off-white w-[100%] border-2 border-transparent border-solid ">
       <div className="mt-[100px] ">
-        <div className="flex justify-between items-start gap-[20px] mx-auto w-[95%] ">
-          <p className="flex flex-col font-helvetica gap-[5px]">
+        <div className="flex justify-between items-start gap-[20px] mx-auto w-[95%] max-[850px]:flex-col ">
+          <p className="flex flex-col font-helvetica gap-[5px] max-[850px]:w-full max-[850px]:text-end">
             <span>ART & CONGRESS</span> <span>HALL </span>{" "}
           </p>
-          <div className="font-miracle w-[520px] max-w-[520px]">
-            <h1 className=" scroll-m-20 text-4xl font-extrabold tracking-[.10em] lg:text-8xl text-text_royal_green">
+          <div className="font-miracle w-[520px] max-w-[520px]  max-[850px]:w-[100%] max-[850px]:max-w-[100%]  gap-[5px] flex flex-col">
+            <h1 className="scroll-m-10 max-[540px]:w-[60%]  max-sm:text-[70px] sm:text-[70px] lg:text-[100px] max-[850px]:text-center font-extrabold tracking-[.10em] text-text_royal_green">
               OUR{" "}
             </h1>
-            <h1 className=" scroll-m-20 text-4xl font-extrabold tracking-[.10em] lg:text-8xl  text-text_royal_green">
+            <h1 className="max-sm:text-[70px] max-[540px]:w-[60%]  leading-[70px] sm:text-[70px] lg:text-[100px] scroll-m-10 max-[850px]:text-center  font-extrabold tracking-[.10em] text-text_royal_green">
               FACI{" "}
             </h1>
             <div className="flex items-center justify-end gap-2">
               <div className="w-[30px]">
                 <img src="./Star.svg" alt="img" />
               </div>
-              <h1 className=" scroll-m-20 text-4xl font-extrabold tracking-[.10em] lg:text-8xl  text-text_royal_green">
+              <h1 className="max-sm:text-[70px] sm:text-[70px] lg:text-[100px] scroll-m-20 font-extrabold tracking-[.10em]  text-text_royal_green">
                 {" "}
                 LITIES{" "}
               </h1>
             </div>
           </div>
           <div className="relative">
-            <div className="">
+            <div className="max-[850px]:hidden">
               <img src="./barman.png" alt="img" />
             </div>
-            <div className="absolute w-[80px] top-[-40px] left-[-30px]">
+            <div className="absolute w-[80px] max-[850px]:hidden min-[850px]:block  top-[-40px] left-[-30px]">
               <img src="./label2.png" alt="img" />
             </div>
           </div>
         </div>
-        <div className="flex items-end mx-auto gap-[50px] w-[95%] mt-[40px]">
-          <div className="w-[400px]">
+        <div className="flex  items-end  mx-auto gap-[70px] w-[95%] mt-[40px] max-[600px]:flex-col  max-[600px]:gap-[50px]">
+          <div className="relative w-[400px] max-[600px]:flex max-[600px]:items-start max-[600px]:w-[60%] min-w-[300px] max-[600px]:mx-auto">
             <img src="./kitchen.png" alt="img" />
+            <div className="absolute w-[80px] top-[-40px] max-[850px]:block min-[850px]:hidden right-[-30px]">
+              <img src="./label2.png" alt="img" />
+            </div>
           </div>
-          <p className="w-[300px] font-helvetica text-text_royal_green">
+          <p className="w-[300px] font-helvetica text-text_royal_green max-[600px]:w-[70%] max-[600px]:flex max-[600px]:justify-end ">
             Bank Hotel offers you a wide range of additional services and
             facilities. Visit our restaurant to try exclusive meals, book a
             conference hall to organize a business meeting, or relax in the
--- components/home/home_Slider/Home_Slider.tsx
+++ components/home/home_Slider/Home_Slider.tsx
@@ -7,50 +7,64 @@ function Home_Slider({}: Props) {
   return (
     <div className="bg-off-white border-2 border-transparent border-solid">
       <div className="h-full lg:my-[100px] max-md:my-[50px] ">
-        <div className="flex justify-between items-start gap-[20px] mx-auto w-[95%] max-[760px]:flex-wrap max-[650px]:justify-center">
-          <h1 className="text-start leading-[65px] flex flex-col gap-[10px] text-text_royal_green font-miracle scroll-m-10 text-[40px] font-medium tracking-[.10em] lg:text-[80px] xl:text-[80px]  max-xl:text-[50px] max-[760px]:text-center max-[760px]:w-full ">
+        <div className="flex justify-between items-start gap-[20px] mx-auto mt-[50px] w-[95%] max-[760px]:flex-wrap max-[650px]:justify-center">
+          <h1 className="text-start leading-[65px] flex flex-col gap-[10px] text-text_royal_green font-miracle scroll-m-10 text-[40px] font-medium tracking-[.10em] lg:text-[80px] xl:text-[80px]  max-xl:text-[50px]  max-[500px]:text-[30px]  max-[500px]:leading-[35px] max-[760px]:text-center max-[760px]:w-full ">
             <span className="max-[760px]:w-[50%] max-[760px]:text-end ">
               {" "}
               ROOMS{" "}
             </span>{" "}
             <span className="max-[760px]:text-end"> & APARTMENTS </span>
           </h1>
-          <p className=" w-[280px]  max-[760px]:w-[40%] font-helvetica text-text_royal_green text-#000 leading-7">
+          <p className=" w-[280px]  max-[760px]:w-[95%] font-helvetica text-text_royal_green text-#000 leading-7">
             ALL ROOM DECORATION WAS MADE WITH ECOLOGICAL CERTIFIED AND SAFE
             MATERIALS.{" "}
           </p>
         </div>
-        <div className="w-[95%] mx-auto mt-[30px] lg:h-[100vh] border-2 border-red-600">
-          <div className="flex items-end gap-[20px] h-full  justify-between  max-[750px]:flex-col-reverse border-2 border-yellow-600">
-            <div className="flex flex-col items-start justify-between gap-3 max-lg:justify-end border-2 border-green-600 border-solid h-[70%]">
+        <div className="w-[95%] mx-auto mt-[30px] lg:h-[100vh] ">
+          <div className="flex items-end gap-[20px] h-full  justify-between  max-[750px]:flex-col-reverse">
+            <div className="flex flex-col items-start justify-between gap-3 max-lg:justify-end  h-[70%]">
               <div className="w-full max-[1200px]:max-w-[100%] max-lg:hidden">
                 <img src="./mirror-image.png" alt="img" />
               </div>
-              <div className="xl:w-[80px] md:w-[60px] w-[40px] cursor-pointer">
+              <div className="xl:w-[80px] md:w-[60px] w-[40px] cursor-pointer max-[750px]:hidden flex">
                 <img src="./btn-wavy-black.png" alt="img" />
               </div>
             </div>
-            <div className="flex flex-col justify-between items-start gap-[30px] h-[70%] w-[30%] max-lg:justify-center border-2 border-blue-600">
+            <div className="flex flex-col justify-between items-start gap-[30px] h-[70%] w-[30%] max-lg:justify-center  max-[750px]:w-full">
               <div className="flex items-center  gap-[20px] py-2 px-[35px] rounded-[20px] w-fit max-lg:hidden border-solid border-2 border-gray-400">
                 <p className="text-[#FCD043] text-[25px] ">
                   {<TbNorthStar />}{" "}
                 </p>
                 <p>1973 </p>
               </div>
-              <section className="flex flex-col items-start justify-evenly  gap-[25px]">
-                <h1 className="text-start text-text_royal_green scroll-m-20 text-2xl font-semibold tracking-tight">
+              <section className="flex flex-col items-start justify-evenly  gap-[25px] max-[750px]:flex  max-[750px]:flex-column-reverse max-[750px]:items-center">
+                <h1 className="text-start flex max-[750px]:hidden text-text_royal_green scroll-m-20 text-2xl font-semibold tracking-tight">
                   Superior Twin
                 </h1>
-                <p className="leading-7 text-text_royal_green font-helvetica  [&:not(:first-child)]:mt-2">
-                  The Superior twin is perfect for those who plan to stay long.
-                  The spacious and bright room is equipped with deluxe Italian
-                  furniture and has a beautiful view to the historical part of
-                  the city. Stylish interior design and comfortable beds will
-                  make your stay cozy and pleasant.
-                </p>
-                <p>
-                  <span>01 / 04</span>
-                </p>
+                <div className="max-[750px]:flex max-[750px]:flex-row-reverse  justify-between items-center">
+                  <div className="flex flex-col gap-[20px] max-[750px]:w-[70%] ">
+                    <h1 className="text-start max-[750px]:flex hidden  text-text_royal_green scroll-m-20 text-2xl font-semibold tracking-tight">
+                      Superior Twin
+                    </h1>
+                    <p className="leading-7 text-text_royal_green font-helvetica  [&:not(:first-child)]:mt-2">
+                      The Superior twin is perfect for those who plan to stay
+                      long. The spacious and bright room is equipped with deluxe
+                      Italian furniture and has a beautiful view to the
+                      historical part of the city. Stylish interior design and
+                      comfortable beds will make your stay cozy and pleasant.
+                    </p>
+                    <p className="max-[750px]:hidden flex">
+                      <span>01 / 04</span>
+                    </p>
+                  </div>
+                  <div className=" hidden  xl:w-[80px] max-xl:w-[60px] cursor-pointer max-[750px]:flex">
+                    <img
+                      className="w-full"
+                      src="./btn-wavy-black.png"
+                      alt="img"
+                    />
+                  </div>
+                </div>
               </section>
             </div>
             <p className="hidden max-[750px]:flex items-center gap-[20px]">
@@ -58,7 +72,7 @@ function Home_Slider({}: Props) {
               <span>/</span>
               <span>04</span>
             </p>
-            <div className="w-[50%] min-w-[400px] max-w-[550px] max-[750px]:max-w-[100%]  max-[750px]:w-[100%] border-2 border-red-500">
+            <div className="w-[50%] min-w-[400px] max-w-[550px] max-[750px]:max-w-[100%]  max-[750px]:min-w-[100%] ">
               <img src="./room-image.png" alt="img" />
             </div>
           </div>
